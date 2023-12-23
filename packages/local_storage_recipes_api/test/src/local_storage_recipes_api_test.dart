2126eb2721eec1bb584bcbec5336b034d9de23d8
chaww <chatchawan.wc@gmail.com>
2023-12-23 17:23:57 +0700

    add unit test LocalStorageRecipesApi

--- packages/local_storage_recipes_api/test/src/local_storage_recipes_api_test.dart
+++ packages/local_storage_recipes_api/test/src/local_storage_recipes_api_test.dart
@@ -1,12 +1,236 @@
 // ignore_for_file: prefer_const_constructors
+import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:local_storage_recipes_api/local_storage_recipes_api.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:recipes_api/recipes_api.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
 
 void main() {
   group('LocalStorageRecipesApi', () {
-    test('can be instantiated', () {
-      expect(LocalStorageRecipesApi(), isNotNull);
+    late SharedPreferences plugin;
+
+    final menuList = [
+      Menu(
+        id: '1',
+        category: 'coffee',
+        image: 'a001.jpg',
+        name: 'อเมริกาโน่',
+        recipeList: [
+          Recipe(
+            name: 'อเมริกาโน่',
+            coffeeType: 'กาแฟคั่วกลาง',
+            type: 'ร้อน',
+            image: 'a001.png',
+            ingredients: [
+              Ingredient(name: 'กาแฟคั่วกลาง', amount: '1', unit: 'ML'),
+              Ingredient(name: 'น้ำร้อน', amount: '2', unit: 'ML'),
+            ],
+          ),
+          Recipe(
+            name: 'อเมริกาโน่',
+            coffeeType: 'กาแฟคั่วกลาง',
+            type: 'เย็น',
+            image: 'a002.png',
+            ingredients: [
+              Ingredient(name: 'กาแฟคั่วกลาง', amount: '3', unit: 'ML'),
+              Ingredient(name: 'น้ำเย็น', amount: '4', unit: 'ML'),
+            ],
+          ),
+          Recipe(
+            name: 'อเมริกาโน่',
+            coffeeType: 'กาแฟคั่วอ่อน',
+            type: 'ร้อน',
+            image: 'a003.png',
+            ingredients: [
+              Ingredient(name: 'กาแฟคั่วอ่อน', amount: '5', unit: 'ML'),
+              Ingredient(name: 'น้ำร้อน', amount: '6', unit: 'ML'),
+            ],
+          ),
+          Recipe(
+            name: 'อเมริกาโน่',
+            coffeeType: 'กาแฟคั่วอ่อน',
+            type: 'เย็น',
+            image: 'a004.png',
+            ingredients: [
+              Ingredient(name: 'กาแฟคั่วอ่อน', amount: '7', unit: 'ML'),
+              Ingredient(name: 'น้ำเย็น', amount: '8', unit: 'ML'),
+            ],
+          ),
+        ],
+      ),
+      Menu(
+        id: '2',
+        category: 'coffee',
+        image: 'b002.jpg',
+        name: 'คาปูชิโน่',
+        recipeList: [
+          Recipe(
+            name: 'คาปูชิโน่',
+            type: 'ร้อน',
+            image: 'b002.jpg',
+            ingredients: [
+              Ingredient(name: 'กาแฟคั่วกลาง', amount: '1', unit: 'ML'),
+              Ingredient(name: 'ฟองนมร้อน', amount: '2', unit: 'ML'),
+            ],
+          )
+        ],
+      ),
+    ];
+
+    setUp(() {
+      plugin = MockSharedPreferences();
+      when(() => plugin.getString(any())).thenReturn(json.encode(menuList));
+      when(() => plugin.setString(any(), any())).thenAnswer((_) async => true);
+    });
+
+    LocalStorageRecipesApi createSubject() {
+      return LocalStorageRecipesApi(
+        plugin: plugin,
+      );
+    }
+
+    group('constructor', () {
+      test('works properly', () {
+        expect(
+          createSubject,
+          returnsNormally,
+        );
+      });
+
+      group('initializes the menuList stream', () {
+        test('with existing menuList if present', () {
+          final subject = createSubject();
+
+          expect(subject.getMenuList(), emits(menuList));
+          verify(
+            () => plugin.getString(
+              LocalStorageRecipesApi.kRecipesCollectionKey,
+            ),
+          ).called(1);
+        });
+
+        test('with empty list if no Menu present', () {
+          when(() => plugin.getString(any())).thenReturn(null);
+
+          final subject = createSubject();
+
+          expect(subject.getMenuList(), emits(const <Menu>[]));
+          verify(
+            () => plugin.getString(
+              LocalStorageRecipesApi.kRecipesCollectionKey,
+            ),
+          ).called(1);
+        });
+      });
+    });
+
+    test('getMenuList returns stream of current list menuList', () {
+      expect(
+        createSubject().getMenuList(),
+        emits(menuList),
+      );
+    });
+
+    group('saveMenu', () {
+      test('saves new Menu', () {
+        final newMenu = Menu(
+          id: '3',
+          category: 'coffee',
+          image: 'b003.jpg',
+          name: 'คอฟฟี่ลาเต้',
+          recipeList: [
+            Recipe(
+              name: 'คอฟฟี่ลาเต้',
+              type: 'ร้อน',
+              image: 'b003.jpg',
+              ingredients: [
+                Ingredient(name: 'กาแฟ', amount: '4', unit: 'ML'),
+                Ingredient(name: 'ฟองนมร้อน', amount: '5', unit: 'ML'),
+              ],
+            )
+          ],
+        );
+
+        final newMenuList = [...menuList, newMenu];
+
+        final subject = createSubject();
+
+        expect(subject.saveMenu(newMenu), completes);
+        expect(subject.getMenuList(), emits(newMenuList));
+
+        verify(
+          () => plugin.setString(
+            LocalStorageRecipesApi.kRecipesCollectionKey,
+            json.encode(newMenuList),
+          ),
+        ).called(1);
+      });
+
+      test('updates existing Menu', () {
+        final updatedMenu = Menu(
+          id: '2',
+          category: 'coffee',
+          image: 'b002.jpg',
+          name: 'คาปูชิโน่',
+          recipeList: [
+            Recipe(
+              name: 'คาปูชิโน่',
+              type: 'ร้อน',
+              image: 'b002.jpg',
+              ingredients: [
+                Ingredient(name: 'กาแฟคั่วกลาง', amount: '10', unit: 'ML'),
+                Ingredient(name: 'ฟองนมร้อน', amount: '20', unit: 'ML'),
+              ],
+            ),
+          ],
+        );
+        final newMenuList = [...menuList.sublist(0, 1), updatedMenu];
+
+        final subject = createSubject();
+
+        expect(subject.saveMenu(updatedMenu), completes);
+        expect(subject.getMenuList(), emits(newMenuList));
+
+        verify(
+          () => plugin.setString(
+            LocalStorageRecipesApi.kRecipesCollectionKey,
+            json.encode(newMenuList),
+          ),
+        ).called(1);
+      });
+
+      group('deleteMenu', () {
+        test('deletes existing Menu', () {
+          final newMenuList = menuList.sublist(1);
+
+          final subject = createSubject();
+
+          expect(subject.deleteMenu(menuList[0].id), completes);
+          expect(subject.getMenuList(), emits(newMenuList));
+
+          verify(
+            () => plugin.setString(
+              LocalStorageRecipesApi.kRecipesCollectionKey,
+              json.encode(newMenuList),
+            ),
+          ).called(1);
+        });
+
+        test(
+          'throws MenuNotFoundException if menu '
+          'with provided id is not found',
+          () {
+            final subject = createSubject();
+            expect(
+              () => subject.deleteMenu('non-existing-id'),
+              throwsA(isA<MenuNotFoundException>()),
+            );
+          },
+        );
+      });
     });
   });
 }
