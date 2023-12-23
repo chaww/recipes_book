8b8554db79c1d16cd65e1f9a535853d468b1b413
chaww <chatchawan.wc@gmail.com>
2023-12-23 17:49:26 +0700

    update models

--- packages/recipes_api/lib/src/models/ingredient.dart
+++ packages/recipes_api/lib/src/models/ingredient.dart
@@ -1,6 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'package:meta/meta.dart';
 import 'package:recipes_api/recipes_api.dart';
 
 part 'ingredient.g.dart';
