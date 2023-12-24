94fdfba929ac5e90db03d0718279ff3fe4b7dc85
chaww <chatchawan.wc@gmail.com>
2023-12-25 01:15:20 +0700

    add RecipesRepository

--- packages/recipes_repository/lib/src/recipes_repository.dart
+++ packages/recipes_repository/lib/src/recipes_repository.dart
@@ -1,7 +1,21 @@
+import 'package:recipes_api/recipes_api.dart';
+
 /// {@template recipes_repository}
 /// A Very Good Project created by Very Good CLI.
 /// {@endtemplate}
 class RecipesRepository {
   /// {@macro recipes_repository}
-  const RecipesRepository();
+  const RecipesRepository({
+    required RecipesApi recipesApi,
+  }) : _recipesApi = recipesApi;
+
+  final RecipesApi _recipesApi;
+
+  Stream<List<Menu>> getBook() => _recipesApi.getBook();
+
+  Future<Menu> getMenu(String id) => _recipesApi.getMenu(id);
+
+  Future<void> saveMenu(Menu menu) => _recipesApi.saveMenu(menu);
+
+  Future<void> deleteMenu(String id) => _recipesApi.deleteMenu(id);
 }
